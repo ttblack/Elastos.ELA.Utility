b275c4a6d455b359f4d9e48ca97a170893e1f1cd
AlexPan <ppgod@live.cn>
2018-09-08 16:00:52 +0800

    initial elalog package implement

--- elalog/logger.go
+++ elalog/logger.go
@@ -1,6 +1,4 @@
-package log
-
-var logger Logger
+package elalog
 
 type Logger interface {
 	Info(e ...interface{})
@@ -20,56 +18,4 @@ type Logger interface {
 
 	Debug(e ...interface{})
 	Debugf(format string, e ...interface{})
-}
-
-func Info(e ...interface{}) {
-	logger.Info(e)
-}
-
-func Infof(format string, e ...interface{}) {
-	logger.Infof(format, e)
-}
-
-func Warn(e ...interface{}) {
-	logger.Warn(e)
-}
-
-func Warnf(format string, e ...interface{}) {
-	logger.Warnf(format, e)
-}
-
-func Error(e ...interface{}) {
-	logger.Error(e)
-}
-
-func Errorf(format string, e ...interface{}) {
-	logger.Errorf(format, e)
-}
-
-func Fatal(e ...interface{}) {
-	logger.Fatal(e)
-}
-
-func Fatalf(format string, e ...interface{}) {
-	logger.Fatalf(format, e)
-}
-
-func Trace(e ...interface{}) {
-	logger.Trace(e)
-}
-
-func Tracef(format string, e ...interface{}) {
-	logger.Tracef(format, e)
-}
-
-func Debug(e ...interface{}) {
-	logger.Debug(e)
-}
-
-func Debugf(format string, e ...interface{}) {
-	logger.Debugf(format, e)
-}
-
-func SetLogger(log Logger) {
-	logger = log
 }
--- elalog/zaplog.go
+++ elalog/zaplog.go
@@ -1,1 +1,79 @@
 package elalog
+
+import (
+	"go.uber.org/zap"
+)
+
+type ZapLogger struct {
+	*zap.SugaredLogger
+}
+
+func NewZapLogger() Logger {
+	logger, _ := zap.NewProduction()
+	return &ZapLogger{
+		SugaredLogger:logger.Sugar(),
+	}
+}
+
+func (z *ZapLogger) Info(e ...interface{}) {
+	defer z.Sync()
+	z.Info(e)
+}
+
+func (z *ZapLogger) Infof(format string, e ...interface{}) {
+	defer z.Sync()
+	z.Infof(format, e)
+}
+
+func (z *ZapLogger) Warn(e ...interface{}) {
+	defer z.Sync()
+	z.Warn(e)
+}
+
+func (z *ZapLogger) Warnf(format string, e ...interface{}) {
+	defer z.Sync()
+	z.Warnf(format, e)
+}
+
+func (z *ZapLogger) Error(e ...interface{}) {
+	defer z.Sync()
+	z.Error(e)
+}
+
+func (z *ZapLogger) Errorf(format string, e ...interface{}) {
+	defer z.Sync()
+	z.Errorf(format, e)
+}
+
+
+func (z *ZapLogger) Fatal(e ...interface{}) {
+	defer z.Sync()
+	z.Fatal(e)
+}
+
+func (z *ZapLogger) Fatalf(format string, e ...interface{}) {
+	defer z.Sync()
+	z.Fatalf(format, e)
+}
+
+
+func (z *ZapLogger) Trace(e ...interface{}) {
+	defer z.Sync()
+	z.Trace(e)
+}
+
+func (z *ZapLogger) Tracef(format string, e ...interface{}) {
+	defer z.Sync()
+	z.Tracef(format, e)
+}
+
+
+func (z *ZapLogger) Debug(e ...interface{}) {
+	defer z.Sync()
+	z.Debug(e)
+}
+
+func (z *ZapLogger) Debugf(format string, e ...interface{}) {
+	defer z.Sync()
+	z.Debugf(format, e)
+}
