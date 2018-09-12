6259ffeaea0030fce8039ae36f1a900e1ed4c277
AlexPan <ppgod@live.cn>
2018-09-12 13:59:24 +0800

    fix memory leak when deserialize for p2p/msg/notfound.go

--- p2p/msg/notfound.go
+++ p2p/msg/notfound.go
@@ -1,10 +1,6 @@
 package msg
 
 import (
-	"fmt"
-	"io"
-
-	"github.com/elastos/Elastos.ELA.Utility/common"
 	"github.com/elastos/Elastos.ELA.Utility/p2p"
 )
 
@@ -12,63 +8,16 @@ import (
 var _ p2p.Message = (*NotFound)(nil)
 
 type NotFound struct {
-	InvList []*InvVect
+	Inv
 }
 
 func NewNotFound() *NotFound {
 	msg := &NotFound{
-		InvList: make([]*InvVect, 0, defaultInvListSize),
+		Inv: *NewInv(),
 	}
 	return msg
 }
 
-// AddInvVect adds an inventory vector to the message.
-func (msg *NotFound) AddInvVect(iv *InvVect) error {
-	if len(msg.InvList)+1 > MaxInvPerMsg {
-		return fmt.Errorf("NotFound.AddInvVect too many invvect in message [max %v]", MaxInvPerMsg)
-	}
-
-	msg.InvList = append(msg.InvList, iv)
-	return nil
-}
-
 func (msg *NotFound) CMD() string {
 	return p2p.CmdNotFound
-}
-
-func (msg *NotFound) MaxLength() uint32 {
-	return 4 + (MaxInvPerMsg * maxInvVectPayload)
-}
-
-func (msg *NotFound) Serialize(writer io.Writer) error {
-	count := uint32(len(msg.InvList))
-	if err := common.WriteElement(writer, count); err != nil {
-		return err
-	}
-
-	for _, vect := range msg.InvList {
-		if err := vect.Serialize(writer); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (msg *NotFound) Deserialize(reader io.Reader) error {
-	var count uint32
-	if err := common.ReadElement(reader, &count); err != nil {
-		return err
-	}
-
-	msg.InvList = make([]*InvVect, 0, count)
-	for i := uint32(0); i < count; i++ {
-		vect := new(InvVect)
-		if err := vect.Deserialize(reader); err != nil {
-			return err
-		}
-		msg.InvList = append(msg.InvList, vect)
-	}
-
-	return nil
 }
